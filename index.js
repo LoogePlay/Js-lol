52d6edc1bd9d1dea098b042b1753f3ffc812d946
LoogePlay <looge.dark@mail.ru>
2021-09-17 15:28:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
--const fs = require("fs");
+const fs = require("fs");
  
 // асинхронное чтение
 fs.readFile("ppl.csv", "utf8", 
@@ -7,19 +7,19 @@ fs.readFile("ppl.csv", "utf8",
                 console.log(data);  // выводим считанные данные
 });
 
-const fs = require('fs');
-const parse = require('csv-parse');
-const async = require('async');
+// const fs = require('fs');
+// const parse = require('csv-parse');
+// const async = require('async');
 
-const inputFile='ppl.csv';
+// const inputFile='ppl.csv';
 
-const parser = parse(gender, function (err, data) {
-  async.eachSeries(data, function (line, callback) {
-    // do something with the line
-    doSomething(line).then(function() {
-      // when processing finishes invoke the callback to move to the next one
-      callback();
-    });
-  })
-});
-fs.createReadStream(inputFile).pipe(parser);
+// const parser = parse(gender, function (err, data) {
+//   async.eachSeries(data, function (line, callback) {
+//     // do something with the line
+//     doSomething(line).then(function() {
+//       // when processing finishes invoke the callback to move to the next one
+//       callback();
+//     });
+//   })
+// });
+// fs.createReadStream(inputFile).pipe(parser);
